3f1d79dd618c2959dde46fefd16cd3e835317b47
viiiaa <via24camadoo@gmail.com>
2022-03-19 18:06:31 +0400

    server code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # client-serveur
+
+this program is used to link the client to the server using a socket.
+
---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+s = socket.socket()
+print ('Socket successfully created')
+port = 56789
+s.bind(('', port))
+print(f'socket binded to port{port}')
+s.listen(5)
+print('Socket is listening')
+while True:
+	c, addr = s.accept()
+	print('Got connection from', addr)
+	message = ('Thank you for connecting')
+	c.send(message.encode())
+	c.close()
+
